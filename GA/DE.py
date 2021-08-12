a66d51c7b674005e9c72b4302b3e5dd780111850
unknown <chenxi46@360buyad.local>
2021-08-12 17:50:58 +0800

    add hurdle

--- GA/DE.py
+++ GA/DE.py
@@ -19,11 +19,18 @@ class SearchStrategy(object):
         self.factor2 = 0.5
         self.pop = [myModel.myModel([random.random() * (self.upper - self.lower) + self.lower
                                      for i in range(self.variableNum)]) for j in range(self.popNum)]
+        self.newPop = []
+        self.hurdles = []
+        self.hurdles.append(sum([i.fitness for i in self.pop]) / self.popNum)
         self.best = self.pop[np.argsort([self.pop[i].fitness for i in range(self.popNum)])[0]]
 
+    # produce the child pop
     def generate_tasks(self):
+        newPop = []
         for i in range(self.popNum):
             indiviual = []
+
+            # mutate
             for k in range(self.variableNum):
                 m = self.pop[i].gene[k]
                 if random.random() < self.mutateFactor:
@@ -33,32 +40,53 @@ class SearchStrategy(object):
                     m = self.upper if m > self.upper else m
                     m = self.lower if m < self.lower else m
                 indiviual.append(m)
-            self.pop.append(myModel.myModel(indiviual))
-        return self.pop
 
+            model = myModel.myModel(indiviual)
+            newPop.append(model)
+        self.newPop = newPop
+        return newPop
+
+    # compute the score and use hurdle to pass the bad child
+    def method_hurdle(self):
+        for i in self.newPop:
+            if not i.compute_score(self.hurdles):
+                del i
+        score = [i.fitness for i in self.newPop]
+        return score
+
+    # par and child together to select and add the hurdle
     def handle_rewards(self, score: List[float]) -> None:
-        index = []
+        score = [i.fitness for i in self.pop] + score
+        pop = self.pop + self.newPop
 
         # tournament algorithm
         n = 2
         randPop = np.random.choice(len(score), len(score), replace=False)
-        red = randPop[:self.popNum]
-        bule = randPop[self.popNum:]
-        index = [red[i] if score[red[i]] < score[bule[i]] else bule[i] for i in range(self.popNum)]
+        red = randPop[:len(score) // 2]
+        blue = randPop[len(score) // 2:]
+        index = [red[i] if score[red[i]] < score[blue[i]] else blue[i] for i in range(len(score) // 2)]
 
-        self.pop = [self.pop[i] for i in index]
-        self.best = self.pop[np.argsort([self.pop[i].fitness for i in range(self.popNum)])[0]]
+        self.pop = [pop[i] for i in index]
+        self.popNum = len(self.pop)
+        self.hurdles.append(sum([i.fitness for i in self.pop]) / self.popNum)
+        self.best = self.pop[np.argsort([i.fitness for i in self.pop])[0]]
         return None
 
 
 if __name__ == '__main__':
     Pop = SearchStrategy()
-    epochs = 100
+    epochs = 50
+    # initial the picture
     p = plot.plot(myModel.object(), Pop.lower, Pop.upper, 200)
-
+    random.seed(time.time())
     for i in range(epochs):
-        random.seed(time.time())
+        # mutate
         Pop.generate_tasks()
-        Pop.handle_rewards([i.fitness for i in Pop.pop])
+
+        # select
+        Pop.handle_rewards(Pop.method_hurdle())
+
+        # draw the 3D picture
         p.point(np.array([j.gene[0] for j in Pop.pop]), np.array([j.gene[1] for j in Pop.pop]))
+
         print(Pop.best.gene, Pop.best.fitness)
--- GA/myModel.py
+++ GA/myModel.py
@@ -6,10 +6,8 @@ def object():
     def general(x, y):
         return pow(x - 1, 2) + pow(y - 1, 2)
 
-
     def rosenbroke(x, y):
         return (x - 1) * (x - 1) + 100 * pow((y - pow(x, 2)), 2)
-
 
     def csdn(x, y):
         return 3 * (1 - x) ** 2 * np.exp(-(x ** 2) - (y + 1) ** 2) - 10 * (x / 5 - x ** 3 - y ** 5) * np.exp(
@@ -17,8 +15,24 @@ def object():
 
     return csdn
 
+
 class myModel:
     def __init__(self, gene):
+        trainNum = 1
         self.num = len(gene)
         self.gene = gene
-        self.fitness = -object()(*gene)
+        self.steps = [(i + 1) * trainNum for i in range(100)]
+        self.step = 0
+        self.fitness = 0
+        for i in range(trainNum):
+            self.fitness = -object()(*gene)
+
+    def compute_score(self, hurdles):
+        while self.step < len(hurdles):
+            for j in range(self.steps[self.step]):
+                self.fitness = -object()(*self.gene)
+            if self.fitness < hurdles[self.step]:
+                self.step += 1
+            else:
+                return False
+        return True
