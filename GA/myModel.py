02500e3c14745944d5527b1014459155c141e8a8
unknown <chenxi46@360buyad.local>
2021-08-12 12:29:43 +0800

    test another function

--- GA/myModel.py
+++ GA/myModel.py
@@ -3,8 +3,7 @@ import numpy as np
 
 
 def object():
-    def general(n):
-        x, y = n
+    def general(x, y):
         return pow(x - 1, 2) + pow(y - 1, 2)
 
 
@@ -12,15 +11,14 @@ def object():
         return (x - 1) * (x - 1) + 100 * pow((y - pow(x, 2)), 2)
 
 
-    def csdn(n):
-        x, y = n
+    def csdn(x, y):
         return 3 * (1 - x) ** 2 * np.exp(-(x ** 2) - (y + 1) ** 2) - 10 * (x / 5 - x ** 3 - y ** 5) * np.exp(
             -x ** 2 - y ** 2) - 1 / 3 ** np.exp(-(x + 1) ** 2 - y ** 2)
 
-    return rosenbroke
+    return csdn
 
 class myModel:
     def __init__(self, gene):
         self.num = len(gene)
         self.gene = gene
-        self.fitness = object()(*gene)
+        self.fitness = -object()(*gene)
